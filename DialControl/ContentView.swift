4358453a9e532efd451b77bf8adada189f3c4628
pakirby1 <phil.kirby@gmail.com>
2020-02-15 03:07:47 -0800

    Initial Commit

---
+++ DialControl/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DialControl
+//
+//  Created by Phil Kirby on 2/15/20.
+//  Copyright © 2020 SoftDesk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
