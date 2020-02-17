e541cf6e6ad9ec9cd8498702e5f5375421bd7c3c
pakirby1 <phil.kirby@gmail.com>
2020-02-16 23:04:37 -0800

    initial

--- DialControl/ContentView.swift
+++ DialControl/ContentView.swift
@@ -7,15 +7,418 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+//        Text("Hello, World!")
+//        DialControl(width: 500, height: 500)
+        TemperatureDial(temperature: 125)
+//        TestView()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct DialControl: View {
+    var segmentCount: UInt = 16
+    var selectedSegments: [UInt] = [6]
+    var width: CGFloat
+    var height: CGFloat
+    var innerRatio = (7 / 8)
+    
+    var body: some View {
+        RotatingCircle(width: width, height: height)
     }
 }
+
+struct RotatingCircle: View {
+    @State var angle = Angle(degrees: 0.0)
+    var width: CGFloat
+    var height: CGFloat
+    @State var xLocation: CGFloat = 0
+    @State var yLocation: CGFloat = 0
+    @State private var currentAmount: Angle = .degrees(0)
+    @State private var finalAmount: Angle = .degrees(0)
+    
+    var rotation: some Gesture {
+        RotationGesture()
+            .onChanged { angle in
+                self.angle = angle
+            }
+    }
+    
+    var drag: some Gesture {
+       DragGesture().onChanged() { value in
+        self.xLocation = value.location.x
+        self.yLocation = value.location.y
+        
+//           let x: CGFloat = min(max(value.location.x, 0), self.innerScale)
+//           let y: CGFloat = min(max(value.location.y, 0), self.innerScale)
+//
+//           let ending = CGPoint(x: x, y: y)
+//           let start = CGPoint(x: (self.innerScale) / 2, y: (self.innerScale) / 2)
+//
+//           let angle = self.angle(between: start, ending: ending)
+//           self.value = CGFloat(Int(((angle / 360) * (self.maxTemperature / self.stepSize)))) / (self.maxTemperature / self.stepSize)
+       }
+    }
+    
+    var body: some View {
+        Rotation()
+    }
+}
+
+// Hold down the option key and drag to rotate
+struct Rotation: View {
+    enum RotationState {
+        case inactive
+        case rotating(angle: Angle)
+        
+        var rotationAngle: Angle {
+            switch self {
+            case .rotating(let angle):
+                return angle
+            default:
+                return Angle.zero
+            }
+        }
+    }
+    
+    var anglePublisher = PassthroughSubject<Angle, Never>()
+    private var cancellables: Set<AnyCancellable> = []
+    
+    @GestureState var rotationState = RotationState.inactive
+    @State var viewRotationState = Angle(degrees: 0.0)
+    
+    var totalRotation: CGFloat = CGFloat()
+    var totalSegments: UInt = 4
+    @State var currentSegment: UInt = 0
+    
+    init() {
+        anglePublisher.sink{ value in
+            print("rotated: \(value.degrees) from top")
+        }
+        .store(in: &cancellables)
+    }
+    
+    var rotationAngle: Angle {
+        print("viewRotationState: \(viewRotationState.degrees) rotationAngle: \(rotationState.rotationAngle.degrees)")
+        var ret = viewRotationState + rotationState.rotationAngle
+        
+        if ret.degrees < 0 {
+            ret = Angle(degrees: 360.0) + ret
+        }
+        
+        anglePublisher.send(ret)
+        
+//        // figure out the segment
+//        let segmentAngle = 360.0 / Double(totalSegments)
+//        currentSegment = UInt(ret.degrees / segmentAngle)
+        
+        return ret
+    }
+    
+    var rotationHistory: [Angle] = []
+    
+    var body: some View {
+        
+        let rotationGesture = RotationGesture(minimumAngleDelta: Angle(degrees: 1))
+            .updating($rotationState) { value, state, transation in
+                state = .rotating(angle: value)
+        }.onEnded { value in
+            self.viewRotationState += value
+            let segmentAngle = 360.0 / Double(self.totalSegments)
+            let tempSegment = self.rotationAngle.degrees / segmentAngle
+//            self.currentSegment = UInt()
+            print("onEnded.value: \(value.degrees) viewRotationState: \(self.viewRotationState.degrees) segmentAngle: \(segmentAngle) tempSegment: \(tempSegment)")
+        }
+        
+        return
+            ZStack {
+                Rectangle()
+                    .fill(Color.gray)
+                    .frame(width: 350, height: 650)
+                    .rotationEffect(rotationAngle)
+                    .gesture(rotationGesture)
+                
+                VStack {
+                    Text("Angle: \(rotationAngle.degrees)").font(.title)
+                    Text("Segment: \(currentSegment)").font(.title)
+//                    Text("\(rotationState.rotationAngle.degrees)").font(.title)
+                }
+            }
+    }
+}
+
+struct TemperatureDial: View {
+
+    @State private var value: CGFloat = 0
+
+    private let initialTemperature: CGFloat
+    private let outerDiameter: CGFloat = 275
+    private let indicatorLength: CGFloat = 25
+    private let maxTemperature: CGFloat = 360
+    private let stepSize: CGFloat = 0.5
+
+    @State var currentSegment: UInt = 0
+    var maneuvers: [String] = ["1LT", "1LB", "1S", "1RB", "2LT", "2LB", "2S", "2RB"]
+    
+    var totalSegments: CGFloat {
+        CGFloat(maneuvers.count)
+    }
+    
+    private var innerDiameter: CGFloat {
+        return outerDiameter - indicatorLength
+    }
+
+
+    init(temperature: CGFloat) {
+        self.initialTemperature = temperature
+    }
+
+    private func angle(between starting: CGPoint, ending: CGPoint) -> CGFloat {
+        let center = CGPoint(x: ending.x - starting.x, y: ending.y - starting.y)
+        let radians = atan2(center.y, center.x)
+        var degrees = 90 + (radians * 180 / .pi)
+
+        if degrees < 0 {
+            degrees += 360
+        }
+
+        return degrees
+    }
+
+    func calculateCurrentSegment(percentage: CGFloat) -> UInt {
+        UInt((self.value * self.totalSegments).rounded(.down))
+    }
+    
+    @State private var currentTemperature : CGFloat = 0.0
+    @State private var rotationAngle: CGFloat = 0.0
+    
+    var textCircleRadius: CGFloat {
+        return (self.innerDiameter / 2) - 25
+    }
+    
+    var dashedStyle : StrokeStyle {
+        StrokeStyle(lineWidth: self.indicatorLength,
+                    lineCap: .butt,
+                    lineJoin: .miter,
+                    dash: [4])
+    }
+    
+    func pointOnCircle(withRadius: CGFloat, withAngle: CGFloat) -> (CGFloat, CGFloat) {
+        let x = withRadius * cos(withAngle)
+        let y = withRadius * sin(withAngle)
+        
+        let points = getCirclePoints(centerPoint: CGPoint(x: 0, y: 0),
+                                     radius: withRadius,
+                                     n: 8)
+        
+        points.forEach{ print($0) }
+        
+        return (x, y)
+    }
+    
+    func getOffsetForIndex(withRadius: CGFloat,
+                           numPoints: Int,
+                           index: Int) -> CGPoint
+    {
+        let points = getCirclePoints(centerPoint: CGPoint(x: 0, y: 0),
+                                     radius: withRadius,
+                                     n: numPoints)
+        
+        return points[index]
+    }
+    
+    func xPoint(_ withRadius: CGFloat, _ withAngle: CGFloat) -> CGFloat {
+        let point = pointOnCircle(withRadius: withRadius, withAngle: withAngle)
+        print("x: \(point.0)")
+        return point.0
+    }
+    
+    func yPoint(_ withRadius: CGFloat, _ withAngle: CGFloat) -> CGFloat {
+        let point = pointOnCircle(withRadius: withRadius, withAngle: withAngle)
+        print("y: \(point.1)")
+        return point.1
+    }
+    
+    var innerCircle: some View {
+        ZStack {
+            ZStack {
+                Circle()
+                    .fill(Color.blue)
+                    .frame(width: self.innerDiameter,
+                           height: self.innerDiameter,
+                           alignment: .center)
+                    .rotationEffect(Angle.degrees(Double(self.currentTemperature)))
+                
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: self.innerDiameter / 3,
+                           height: self.innerDiameter / 3,
+                           alignment: .center)
+                    .rotationEffect(Angle.degrees(Double(self.currentTemperature)))
+                
+//                Text("1")
+//                    .font(.title)
+//                    .offset(x: 0, y: -self.textCircleRadius)
+//                    .rotationEffect(Angle.degrees(Double(self.currentTemperature)))
+//
+                Text("2")
+                    .font(.title)
+                    .rotated(Angle.degrees(90.0))
+                    .offset(x: xPoint(self.textCircleRadius, 90.0),
+                            y: yPoint(self.textCircleRadius, 90.0))
+                    .rotationEffect(Angle.degrees(Double(self.currentTemperature)))
+                
+                ForEach(self.maneuvers, id:\.self) { maneuver in
+                    let point = getOffsetForIndex(withRadius: CGFloatself.textCircleRadius, numPoints: maneuvers.count, index: 0)
+                    Text("2")
+                    
+                }
+                                
+//                Text("3")
+//                    .font(.title)
+//                    .rotated(Angle.degrees(60))
+////                    .offset(x: xPoint(self.textCircleRadius, 60.0),
+////                            y: yPoint(self.textCircleRadius, 60.0))
+//                    .rotationEffect(Angle.degrees(Double(self.currentTemperature)))
+                
+//                Text("6")
+//                    .font(.title)
+//                    .offset(x: 0, y: self.textCircleRadius)
+//                    .rotationEffect(Angle.degrees(Double(self.currentTemperature)))
+            }
+            .gesture(
+                    DragGesture().onChanged() { value in
+
+                        let x: CGFloat = min(max(value.location.x, 0), self.innerDiameter)
+                        let y: CGFloat = min(max(value.location.y, 0), self.innerDiameter)
+
+                        let ending = CGPoint(x: x, y: y)
+                        let start = CGPoint(x: (self.innerDiameter) / 2, y: (self.innerDiameter) / 2)
+
+                        let angle = self.angle(between: start, ending: ending)
+                        self.value = CGFloat(Int(((angle / 360) * (self.maxTemperature / self.stepSize)))) / (self.maxTemperature / self.stepSize)
+                        
+                        self.currentSegment = self.calculateCurrentSegment(percentage: self.value)
+                        
+                        self.currentTemperature = self.value * self.maxTemperature
+                    }
+                )
+        
+            Text("\(currentTemperature, specifier: "%.1f") \u{2103} \n segment: \(currentSegment) \n \(maneuvers[Int(currentSegment)])")
+                .font(.largeTitle)
+                .foregroundColor(Color.white)
+                .fontWeight(.semibold)
+            
+            
+        }
+    }
+    
+    var body: some View {
+        ZStack(alignment: .center) {
+            innerCircle
+            
+            // remaining temp
+            Circle()
+                .stroke(Color.blue, style: dashedStyle)
+                .frame(width: self.outerDiameter, height: self.outerDiameter, alignment: .center)
+            
+            // current temp
+            Circle()
+                .trim(from: 0.0, to: self.value)
+                .stroke(Color.red, style: dashedStyle)
+                .rotationEffect(.degrees(-90))
+                .frame(width: self.outerDiameter, height: self.outerDiameter, alignment: .center)
+        }
+        .onAppear(perform: {
+            let percentage = self.initialTemperature / self.maxTemperature
+            self.value = percentage
+            self.currentSegment = self.calculateCurrentSegment(percentage: self.value)
+        })
+    }
+}
+
+struct TestView: View {
+    @State private var rotation = 0.0
+
+    var body: some View {
+        VStack {
+            Slider(value: $rotation, in: 0...360, step: 1.0)
+            Text("Up we go")
+                .rotationEffect(.degrees(rotation), anchor: .topLeading)
+        }
+    }
+}
+
+// https://stackoverflow.com/questions/58494193/swiftui-rotationeffect-framing-and-offsetting
+private struct SizeKey: PreferenceKey {
+    static let defaultValue: CGSize = .zero
+    
+    static func reduce(value: inout CGSize,
+                       nextValue: () -> CGSize)
+    {
+        value = nextValue()
+    }
+}
+
+extension View {
+    func captureSize(in binding: Binding<CGSize>) -> some View {
+        overlay(GeometryReader { proxy in
+            Color.clear.preference(key: SizeKey.self, value: proxy.size)
+        })
+        .onPreferenceChange(SizeKey.self) { size in
+            binding.wrappedValue = size
+        }
+    }
+}
+
+struct Rotated<Rotated: View>: View {
+    var view: Rotated
+    var angle: Angle
+
+    init(_ view: Rotated, angle: Angle = .degrees(-90)) {
+        self.view = view
+        self.angle = angle
+    }
+
+    @State private var size: CGSize = .zero
+
+    var body: some View {
+        // Rotate the frame, and compute the smallest integral frame that contains it
+        let newFrame = CGRect(origin: .zero, size: size)
+            .offsetBy(dx: -size.width/2, dy: -size.height/2)
+            .applying(.init(rotationAngle: CGFloat(angle.radians)))
+            .integral
+        
+        let v = view
+            .fixedSize()                    // Don't change the view's ideal frame
+            .captureSize(in: $size)         // Capture the size of the view's ideal frame
+            .rotationEffect(angle)          // Rotate the view
+            .frame(width: newFrame.width,   // And apply the new frame
+                   height: newFrame.height)
+        
+        return v
+    }
+}
+
+extension View {
+    func rotated(_ angle: Angle = .degrees(-45)) -> some View {
+        Rotated(self, angle: angle)
+    }
+}
+
+func getCirclePoints(centerPoint point: CGPoint, radius: CGFloat, n: Int) -> [CGPoint] {
+//    let points = getCirclePoints(centerPoint: CGPoint(x: 160, y: 240), radius: 120.0, n: 12)
+    
+    let result: [CGPoint] = stride(from: 0.0, to: 360.0, by: Double(360 / n)).map {
+        let bearing = CGFloat($0) * .pi / 180
+        let x = point.x + radius * cos(bearing)
+        let y = point.y + radius * sin(bearing)
+        return CGPoint(x: x, y: y)
+    }
+    
+    return result
+}
+
+
