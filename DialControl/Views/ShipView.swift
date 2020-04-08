7a17fc8f0c58c4951f11aee7272c3a60194252e8
pakirby1 <phil.kirby@gmail.com>
2020-04-08 04:32:23 -0700

    2nd pass at downloading images (synchronously)

--- DialControl/Views/ShipView.swift
+++ DialControl/Views/ShipView.swift
@@ -71,14 +71,31 @@ struct ShipView: View {
             
             HStack(alignment: .center) {
 //                clearView
+//                Image("Card_Pilot_103")
+                
+                // url: URL("https://sb-cdn.fantasyflightgames.com/card_art/Card_art_XW_P_103.jpg")
+                
+                // Naming collision with URLImageView
+//                URLImageView(url: URL(fileURLWithPath: getShipImageUrl(shipName: "tieininterceptor", pilotName: "turrphennir")),
+//                             view: Text("Loading..."))
+                
+//                URLImageView(url: URL(fileURLWithPath: "https://sb-cdn.fantasyflightgames.com/card_images/Card_Pilot_103.png"), view: Text("Loading..."))
+                
+//                URLImageView(url: URL(fileURLWithPath: "https://image.tmdb.org/t/p/original/pThyQovXQrw2m0s9x82twj48Jq4.jpg"), view: Text("Loading..."))
+                
+                Image(uiImage: fetchImageFromURL(urlString: getShipImageUrl(shipName: squadPilot.ship, pilotName: "sabersquadronace")))
+                .resizable()
+//                .aspectRatio(UIImage(named: "Card_Pilot_103")!.size, contentMode: .fit)
+                .frame(width: 350.0,height:500)
+                .border(Color.green, width: 2)
+                .onTapGesture { self.showCardOverlay.toggle() }
+                .overlay( TextOverlay(isShowing: self.$showCardOverlay) )
+                
+//                PAKTestView(url: URL("https://"))
 //                Image(uiImage: getShipImage(shipName: squadPilot.ship, pilotName: squadPilot.name))
-                Image(uiImage: getShipImage(shipName: squadPilot.ship, pilotName: "turrphennir"))
-                    .resizable()
-                    .aspectRatio(UIImage(named: "Card_Pilot_103")!.size, contentMode: .fit)
-                    .frame(width: 350.0,height:500)
-                    .border(Color.green, width: 2)
-                    .onTapGesture { self.showCardOverlay.toggle() }
-                    .overlay( TextOverlay(isShowing: self.$showCardOverlay) )
+//          Image(uiImage: getShipImage(shipName: squadPilot.ship, pilotName: "turrphennir"))
+//                URLImageView(url: getShipImage(shipName: squadPilot.ship, pilotName: "turrphennir"))
+//
                 
                 VStack(spacing: 20) {
                     LinkedView(maxCount: 8, type: StatButtonType.force)
@@ -96,7 +113,37 @@ struct ShipView: View {
     }
     
     //    "tieskstriker"
-    func getShipImage(shipName: String, pilotName: String) -> UIImage {
+    func getShipImageUrl(shipName: String, pilotName: String) -> String {
+        func buildUrl(shipName: String, pilotName: String) -> String {
+            var shipJSON: String = ""
+            
+            print("shipName: \(shipName)")
+            print("pilotName: \(pilotName)")
+            
+            if let pilotFileUrl = shipLookupTable[shipName] {
+                print("pilotFileUrl: \(pilotFileUrl)")
+                
+                if let path = Bundle.main.path(forResource: pilotFileUrl.fileName,
+                                               ofType: "json",
+                                               inDirectory: pilotFileUrl.directoryPath)
+                {
+                    print("path: \(path)")
+                    
+                    do {
+                        shipJSON = try String(contentsOfFile: path)
+                        print("jsonData: \(shipJSON)")
+                    } catch {
+                        print("error reading from \(path)")
+                    }
+                }
+            }
+            
+            let ship: Ship = Ship.serializeJSON(jsonString: shipJSON)
+            let foundPilots: Pilot = ship.pilots.filter{ $0.xws == pilotName }[0]
+            
+            return foundPilots.image
+        }
+        
         func fetchImage(from urlString: String,
                         completionHandler: @escaping (_ data: Data?) -> ())
         {
@@ -117,53 +164,28 @@ struct ShipView: View {
             dataTask.resume()
         }
         
-        var shipJSON: String = ""
+        let url = buildUrl(shipName: shipName, pilotName: pilotName)
         
-        print("shipName: \(shipName)")
-        print("pilotName: \(pilotName)")
-        
-        if let pilotFileUrl = shipLookupTable[shipName] {
-            print("pilotFileUrl: \(pilotFileUrl)")
-            
-            if let path = Bundle.main.path(forResource: pilotFileUrl.fileName,
-                                           ofType: "json",
-                                           inDirectory: pilotFileUrl.directoryPath)
-            {
-                print("path: \(path)")
-                
-                do {
-                    shipJSON = try String(contentsOfFile: path)
-                    print("jsonData: \(shipJSON)")
-                } catch {
-                    print("error reading from \(path)")
-                }
-            }
-        }
-        
-        let ship: Ship = Ship.serializeJSON(jsonString: shipJSON)
-        let foundPilots: Pilot = ship.pilots.filter{ $0.xws == pilotName }[0]
-        var image: UIImage?
-        
-        fetchImage(from: foundPilots.image) { (imageData) in
-            if let data = imageData {
-                // referenced imageView from main thread
-                // as iOS SDK warns not to use images from
-                // a background thread
-                DispatchQueue.main.async {
-                    image = UIImage(data: data)
-                }
-            } else {
-                    // show as an alert if you want to
-                print("Error loading image");
-            }
-        }
+//        fetchImage(from: foundPilots.image) { (imageData) in
+//            if let data = imageData {
+//                // referenced imageView from main thread
+//                // as iOS SDK warns not to use images from
+//                // a background thread
+//                DispatchQueue.main.async {
+//                    image = UIImage(data: data)
+//                }
+//            } else {
+//                    // show as an alert if you want to
+//                print("Error loading image");
+//            }
+//        }
         
 //        print("image Url: \(pilot.image)")
         
 //        let imageName = "" // your image name here
 //        let imagePath: String = "\(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0])/\(imageName).png"
 //        let imageUrl: URL = URL(fileURLWithPath: imagePath)
-        return image ?? UIImage(named: "Card_Pilot_103")!
+        return url
     }
     
     var shipLookupTable: [String:PilotFileUrl] = [
@@ -214,3 +236,107 @@ struct TextOverlay: View {
             .opacity(self.isShowing ? 1 : 0)
     }
 }
+
+/*
+ func asyncMethod(completion: ((String) -> Void)) {
+     //...
+ }
+
+ func promisifiedAsyncMethod() -> AnyPublisher<String, Never> {
+     Future<String, Never> { promise in
+         asyncMethod { value in
+             promise(.success(value))
+         }
+     }
+     .eraseToAnyPublisher()
+ }
+ */
+
+/// https://theswiftdev.com/how-to-download-files-with-urlsession-using-combine-publishers-and-subscribers/
+/// https://www.vadimbulavin.com/asynchronous-swiftui-image-loading-from-url-with-combine-and-swift/
+// Fetches an image from an url and publishes the UIImage
+// on a Combine publisher
+class ImageFetcher : ObservableObject {
+    @Published var image: UIImage?
+    var url: URL
+    
+    init(url: URL) {
+        self.url = url
+    }
+    
+    private var cancellable: AnyCancellable?
+    
+    deinit {
+        cancellable?.cancel()
+    }
+
+    func load() {
+        print("url: \(url.absoluteString)")
+        
+        cancellable = URLSession.shared.dataTaskPublisher(for: url)
+            .map {
+                print("\($0.data.count)")
+                return UIImage(data: $0.data)
+            }
+            .replaceError(with: nil)
+            .receive(on: DispatchQueue.main)
+            .assign(to: \.image, on: self)
+    }
+    
+    func cancel() {
+        cancellable?.cancel()
+    }
+}
+
+// View that references the ImageFetcher
+// let url = URL(string: "https://image.tmdb.org/t/p/original/pThyQovXQrw2m0s9x82twj48Jq4.jpg")!
+// URLImageView(url: URL(string: "https://image.tmdb.org/t/p/original/pThyQovXQrw2m0s9x82twj48Jq4.jpg")!, view: Text("Loading.."))
+struct URLImageView<T: View>: View {
+//    var url: URL
+    @ObservedObject private var imageFetcher: ImageFetcher
+    private let placeholder: T?
+
+    private var image: some View {
+        Group {
+            if imageFetcher.image != nil {
+                Image(uiImage: imageFetcher.image!)
+                    .resizable()
+            } else {
+                placeholder
+            }
+        }
+    }
+    
+    var body: some View {
+        image.onAppear(perform: imageFetcher.load)
+//        EmptyView()
+    }
+    
+    init(url: URL, view: T? = nil) {
+        self.imageFetcher = ImageFetcher(url: url)
+        self.placeholder = view
+    }
+}
+
+func fetchImageFromURL(urlString: String) -> UIImage {
+    var image: UIImage? = nil
+    
+    let url = URL(string: urlString)!
+
+    // Synchronous download using Data & String
+    do {
+        // get the content as String synchronously
+//        let content = try String(contentsOf: url)
+//        print(content)
+
+        // get the content of the url as Data synchronously
+        let data = try Data(contentsOf: url)
+        image = UIImage(data: data)
+    }
+    catch {
+        print(error.localizedDescription)
+    }
+    
+    return image!
+}
+
