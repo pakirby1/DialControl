7102617bdfa651e7fe83e05834b433933c72ab7c
pakirby1 <phil.kirby@gmail.com>
2020-06-29 00:39:04 -0700

    m.c.

--- DialControl/Views/FactionSquadList.swift
+++ DialControl/Views/FactionSquadList.swift
@@ -9,9 +9,21 @@
 import Foundation
 import SwiftUI
 
+class FactionSquadListViewModel : ObservableObject {
+    @Published var squadNames : [String] = []
+    
+    init() {
+        self.squadNames = ["Worlds Champion 2019",
+                       "SaiDuchessTurr3Academies",
+                       "VaderSoontirEcho",
+                       "VagabondMaarekFifthBrotherTurr"]
+    }
+}
+
 struct FactionSquadList: View {
     @EnvironmentObject var viewFactory: ViewFactory
     let faction: String
+    let viewModel = FactionSquadListViewModel()
     
     var body: some View {
         VStack {
@@ -51,52 +63,69 @@ struct FactionSquadList: View {
     
     var squadList: some View {
         VStack {
-            FactionSquadCard(name: "Worlds Champion 2019").environmentObject(viewFactory)
-            FactionSquadCard(name: "SaiDuchessTurr3Academies").environmentObject(viewFactory)
-            FactionSquadCard(name: "VaderSoontirEcho").environmentObject(viewFactory)
-            FactionSquadCard(name: "VagabondMaarekFifthBrotherTurr").environmentObject(viewFactory)
+            ForEach(self.viewModel.squadNames, id:\.self) { name in
+                FactionSquadCard(viewModel: FactionSquadCardViewModel(name: name)).environmentObject(self.viewFactory)
+            }
         }.padding(10)
+    }
+}
+
+class FactionSquadCardViewModel : ObservableObject {
+    let points: Int = 200
+    let name: String
+    let theme: Theme = WestworldUITheme()
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    var buttonBackground: Color {
+        theme.BUTTONBACKGROUND
+    }
+    
+    var textForeground: Color {
+        theme.TEXT_FOREGROUND
+    }
+    
+    var border: Color {
+        theme.BORDER_INACTIVE
     }
 }
 
 struct FactionSquadCard: View {
     @EnvironmentObject var viewFactory: ViewFactory
-    let theme: Theme = WestworldUITheme()
+    let viewModel: FactionSquadCardViewModel
     
-    let points: Int = 200
-    let name: String
+    init(viewModel: FactionSquadCardViewModel) {
+        self.viewModel = viewModel
+    }
     
     var background: some View {
         RoundedRectangle(cornerRadius: 15)
-            .fill(theme.BUTTONBACKGROUND)
+            .fill(viewModel.buttonBackground)
             .frame(width: 800, height: 80)
             .overlay(border)
-        
-//            .stroke(lineWidth: 3.0)
-        
-//            .foregroundColor(Color.blue)
-//
     }
     
     var pointsView: some View {
-        Text("\(points)")
+        Text("\(viewModel.points)")
             .font(.title)
-            .foregroundColor(theme.TEXT_FOREGROUND)
+            .foregroundColor(viewModel.textForeground)
             .padding()
             .background(Color.blue)
             .clipShape(Circle())
     }
     
     var nameView: some View {
-        Text(name)
+        Text(viewModel.name)
             .font(.title)
             .lineLimit(1)
-            .foregroundColor(theme.TEXT_FOREGROUND)
+            .foregroundColor(viewModel.textForeground)
     }
     
     var border: some View {
         RoundedRectangle(cornerRadius: 15)
-            .stroke(theme.BORDER_INACTIVE, lineWidth: 3)
+            .stroke(viewModel.border, lineWidth: 3)
     }
     
     var body: some View {
