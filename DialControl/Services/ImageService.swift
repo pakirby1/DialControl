caac59bfc3608b3277724c98c4e07c071984620c
pakirby1 <phil.kirby@gmail.com>
2021-07-08 01:27:03 -0700

    m.c.

--- DialControl/Services/ImageService.swift
+++ DialControl/Services/ImageService.swift
@@ -164,3 +164,84 @@ struct DownloadImageEvent: CustomStringConvertible {
         return .success(DownloadImageEvent(index: 0, total: 0, url: "", isCompleted: true))
     }
 }
+
+// https://www.swiftbysundell.com/articles/handling-loading-states-in-swiftui/
+enum LoadingState<Value> {
+    case idle
+    case loading
+    case failed(Error)
+    case loaded(Value)
+}
+
+protocol LoadableObject: ObservableObject {
+    associatedtype Output
+    var state: LoadingState<Output> { get }
+    func load()
+}
+
+struct AsyncContentView<Source: LoadableObject,
+                        LoadingView: View,
+                        Content: View>: View {
+    @ObservedObject var source: Source
+    var loadingView: LoadingView
+    var content: (Source.Output) -> Content
+
+    init(source: Source,
+         loadingView: LoadingView,
+         @ViewBuilder content: @escaping (Source.Output) -> Content) {
+        self.source = source
+        self.loadingView = loadingView
+        self.content = content
+    }
+
+    var body: some View {
+        switch source.state {
+        case .idle:
+            Color.clear.onAppear(perform: source.load)
+        case .loading:
+            loadingView
+        case .failed(let error):
+            EmptyView()
+        case .loaded(let output):
+            content(output)
+        }
+    }
+}
+
+class PublishedObject<Wrapped: Publisher>: LoadableObject {
+    @Published private(set) var state = LoadingState<Wrapped.Output>.idle
+
+    private let publisher: Wrapped
+    private var cancellable: AnyCancellable?
+
+    init(publisher: Wrapped) {
+        self.publisher = publisher
+    }
+
+    func load() {
+        state = .loading
+
+        cancellable = publisher
+            .map(LoadingState.loaded)
+            .catch { error in
+                Just(LoadingState.failed(error))
+            }
+            .sink { [weak self] state in
+                self?.state = state
+            }
+    }
+}
+
+/*
+extension AsyncContentView {
+    init<P: Publisher>(
+        source: P,
+        @ViewBuilder content: @escaping (P.Output) -> Content
+    ) where Source == PublishedObject<P> {
+        self.init(
+            source: PublishedObject(publisher: source),
+            content: content
+        )
+    }
+}
+*/
