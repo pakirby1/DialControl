11599b06601a232592728bb81483d230768758d6
pakirby1 <phil.kirby@gmail.com>
2022-07-23 05:29:49 -0700

    m.c.

--- DialControl/Views/ViewPropertiesRepresentable.swift
+++ DialControl/Views/ViewPropertiesRepresentable.swift
@@ -45,3 +45,81 @@ extension Store : StateRepresentable {
     }
 }
 
+/// Can't we just make a version that would directly connect the view with the store in a simpe way?
+/*
+ 
+ 
+public protocol ConnectedView: View {
+    associatedtype State: FluxState
+    associatedtype Props
+    associatedtype V: View
+    
+    func map(state: State, dispatch: @escaping DispatchFunction) -> Props
+    func body(props: Props) -> V
+}
+
+public extension ConnectedView {
+    func render(state: State, dispatch: @escaping DispatchFunction) -> V {
+        let props = map(state: state, dispatch: dispatch)
+        return body(props: props)
+    }
+    
+    var body: StoreConnector<State, V> {
+        return StoreConnector(content: render)
+    }
+}
+ */
+
+protocol StoreConnected: View {
+    associatedtype State
+    associatedtype ViewProperties
+    associatedtype V: View
+    associatedtype Action
+    
+    func buildViewProperties(state: State) -> ViewProperties
+    func body(properties: ViewProperties) -> V
+}
+
+extension StoreConnected {
+    var body: StoreProvider<State, Action, V> {
+        return StoreProvider(content: render)
+    }
+    
+    func render(state: State, dispatch: @escaping (Action) -> Void) -> V {
+        let props: ViewProperties = buildViewProperties(state: state)
+        
+        return body(properties: props)
+    }
+}
+
+struct StoreProvider<State, Action, V:View>: View {
+    @EnvironmentObject var store: Store<State, Action, MyEnvironment>
+    
+    let content: (State, @escaping (Action) -> Void) -> V
+    
+    var body: V {
+        content(store.state, store.send)
+    }
+}
+
+//public struct StoreConnector<State: FluxState, V: View>: View {
+//    @EnvironmentObject var store: Store<State>
+//    let content: (State, @escaping (Action) -> Void) -> V
+//
+//    public var body: V {
+//        content(store.state, store.dispatch(action:))
+//    }
+//}
+
+struct StoreConnectorTestView : StoreConnected {
+    typealias ViewProperties = String
+    typealias Action = MyAppAction
+    
+    func buildViewProperties(state: String) -> ViewProperties {
+        ""
+    }
+    
+    func body(properties: String) -> AnyView {
+        Text("StoreConnectorTestView").eraseToAnyView()
+    }
+}
